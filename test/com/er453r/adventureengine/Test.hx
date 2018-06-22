b9df5bfd7eb111057a260618623a9504b092b064
er453r <er453r@gmail.com>
2018-06-23 00:15:50 +0200

    better project structure

--- test/com/er453r/adventureengine/Test.hx
+++ test/com/er453r/adventureengine/Test.hx
@@ -1,6 +1,6 @@
 package com.er453r.adventureengine;
 
-@script("/home/mkotz/IdeaProjects/ae/adventureengine/test.md")
+@script("test.md")
 class Test extends Adventure{
 	public static function main(){
 		trace("ADventure Engine test Start");
