bd41a16be603b5d4f6453da0ac83d67ea84df3f8
er453r <er453r@gmail.com>
2018-06-23 00:34:42 +0200

    splitted builder and parser

---
+++ src/com/er453r/adventureengine/AdventureParser.hx
@@ -0,0 +1,53 @@
+package com.er453r.adventureengine;
+
+class AdventureParser {
+    private static inline var TITLE_REGEX = "^# (.+)";
+    private static inline var SECTION_REGEX = "^## (.+)";
+    private static inline var CHARACTER_REGEX = "### (.+)";
+    private static inline var EMPTY_REGEX = "^[ \\s\\t]*$";
+
+    public var characters:Array<Character> = [];
+
+    public function new(script:String){
+        var lines = script.split("\n");
+
+        trace('Have ${lines.length} lines');
+
+        for(n in 0...lines.length){
+            var regex = new EReg(TITLE_REGEX, "");
+
+            if(regex.match(lines[n])){
+                trace('Title: ${regex.matched(1)}');
+
+                continue;
+            }
+
+            regex = new EReg(SECTION_REGEX, "");
+
+            if(regex.match(lines[n])){
+                trace('Section: ${regex.matched(1)}');
+
+                continue;
+            }
+
+            regex = new EReg(CHARACTER_REGEX, "");
+
+            if(regex.match(lines[n])){
+                var name = regex.matched(1);
+
+                trace('Sub-section: ${name}');
+
+                characters.push(new Character(name, regex.matchedRight()));
+
+                continue;
+            }
+
+            regex = new EReg(EMPTY_REGEX, "");
+
+            if(regex.match(lines[n])) // do nothing on empty
+                continue;
+
+            trace('Content: ${lines[n]}');
+        }
+    }
+}
--- src/com/er453r/adventureengine/macros/AdventureBuilder.hx
+++ src/com/er453r/adventureengine/macros/AdventureBuilder.hx
@@ -11,12 +11,6 @@ using hx.strings.Strings; // augment all Strings with new functions
 
 class AdventureBuilder {
     private static inline var SCRIPT_ANNOTATION = "script";
-    private static inline var CONTENTS = "contents";
-
-    private static inline var TITLE_REGEX = "^# (.+)";
-    private static inline var SECTION_REGEX = "^## (.+)";
-    private static inline var CHARACTER_REGEX = "### (.+)";
-    private static inline var EMPTY_REGEX = "^[ \\s\\t]*$";
 
     public static function build():Array<Field> {
         var fields:Array<Field> = Context.getBuildFields();
@@ -32,52 +26,15 @@ class AdventureBuilder {
 
         var content = File.getContent(Context.resolvePath(file));
 
-        //trace('Script: $content');
+        var adventure:AdventureParser = new AdventureParser(content);
 
-        var lines = content.split("\n");
-
-        trace('Have ${lines.length} lines');
-
-        for(n in 0...lines.length){
-            var regex = new EReg(TITLE_REGEX, "");
-
-            if(regex.match(lines[n])){
-                trace('Title: ${regex.matched(1)}');
-
-                continue;
-            }
-
-            regex = new EReg(SECTION_REGEX, "");
-
-            if(regex.match(lines[n])){
-                trace('Section: ${regex.matched(1)}');
-
-                continue;
-            }
-
-            regex = new EReg(CHARACTER_REGEX, "");
-
-            if(regex.match(lines[n])){
-                var name = regex.matched(1);
-
-                trace('Sub-section: ${name}');
-
-                fields.push({
-                    name: name.toUpperCamel(),
-                    access: [Access.APublic, Access.AStatic],
-                    kind: FieldType.FVar(macro:Character, macro new Character($v{name}, $v{regex.matchedRight()})),
-                    pos: Context.currentPos()
-                });
-
-                continue;
-            }
-
-            regex = new EReg(EMPTY_REGEX, "");
-
-            if(regex.match(lines[n])) // do nothing on empty
-                continue;
-
-            trace('Content: ${lines[n]}');
+        for(character in adventure.characters){
+            fields.push({
+                name: character.name.toUpperCamel(),
+                access: [Access.APublic, Access.AStatic],
+                kind: FieldType.FVar(macro:Character, macro new Character($v{character.name}, $v{character.description})),
+                pos: Context.currentPos()
+            });
         }
 
         return fields;
