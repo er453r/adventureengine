a1e449c01ec4f4690589f7f749de5e229e40c0b5
er453r <er453r@gmail.com>
2018-06-23 00:48:41 +0200

    some test refactorings

--- test/com/er453r/adventureengine/Test.hx
+++ test/com/er453r/adventureengine/Test.hx
@@ -1,6 +1,6 @@
 package com.er453r.adventureengine;
 
-@script("script-test.md")
+@script("test-script.md")
 class Test extends Adventure{
 	public static function main(){
 		trace("ADventure Engine test Start");
