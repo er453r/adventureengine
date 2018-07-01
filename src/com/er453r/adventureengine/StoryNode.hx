855589d65fb60db254b36fd5a7bdbf9295585b17
er453r <er453r@gmail.com>
2018-07-01 21:51:23 +0200

    more work on branching

--- src/com/er453r/adventureengine/StoryNode.hx
+++ src/com/er453r/adventureengine/StoryNode.hx
@@ -2,20 +2,20 @@ package com.er453r.adventureengine;
 
 class StoryNode {
     private var nodes:Array<StoryNode> = [];
-
     private var content:String;
     private var parent:StoryNode;
 
     public var fork:Bool = false;
+    private var merge:Bool = false;
+
+    public function new(content:String) {
+        this.content = content;
+    }
 
     public function getParent():StoryNode {
         trace('[$content] PARENT - $parent');
 
         return parent;
-    }
-
-    public function new(content:String) {
-        this.content = content;
     }
 
     public function add(storyNode:StoryNode){
@@ -28,10 +28,6 @@ class StoryNode {
         nodes.push(storyNode);
     }
 
-    public function toString(){
-        return '[StoryNode | $content]';
-    }
-
     public function dump(indent:Int = 0, par:String = ""){
         var prefix = "";
 
@@ -39,11 +35,15 @@ class StoryNode {
             prefix += ">";
 
         if(indent > 0)
-            prefix += ' [$par]';
+            prefix += ' [$par] ';
 
         trace(prefix + content);
 
         for(node in nodes)
             node.dump(indent + (fork ? 1 : 0), content);
+    }
+
+    public function toString(){
+        return '[StoryNode | $content]';
     }
 }
